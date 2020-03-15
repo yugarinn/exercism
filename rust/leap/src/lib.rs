809316724ca5f8eb02a262138341cf0e5ec3035b
Sergio Uve <sergiouve@gmail.com>
2020-03-15 23:21:36 +0100

    Add rust leap

---
+++ rust/leap/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn is_leap_year(year: u64) -> bool {
+    if year % 400 == 0 {
+        return true;
+    }
+
+    if year % 4 == 0  && year % 100 != 0 {
+        return true;
+    }
+
+    return false;
+}

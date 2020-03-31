278104320893a77240489f8a0ae139f9cdca1ab5
Sergio Uve <sergiouve@gmail.com>
2020-03-31 22:59:43 +0200

    Refactor javascript resistor-color-duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,7 +1,5 @@
 const BANDS  = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
-const getColorIndex = (color) => BANDS.indexOf(color);
-
 export const decodedValue = (colors) => {
-  return getColorIndex(colors[0]) * 10 + getColorIndex(colors[1]);
-};
+  return BANDS.indexOf(colors[0]) * 10 + BANDS.indexOf(colors[1]);
+}
